be9df07e449e1d955e80a54f5e2d1ceecfbae2a5
moinakghos <moinakghos@gmail.com>
2025-01-22 01:58:59 +0530

    Genesis

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD4W29DBXR18opzTEZnEU2JNQTwt_32QnI",
+    authDomain: "mymakaut-c1246.firebaseapp.com",
+    projectId: "mymakaut-c1246",
+    storageBucket: "mymakaut-c1246.firebasestorage.app",
+    messagingSenderId: "1037242765675",
+    appId: "1:1037242765675:web:90e55c518704d3b6177c2e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
