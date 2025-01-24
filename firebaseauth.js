20791205ef94bd01199b35935b1cad9f2987f1b3
moinakghos <moinakghos@gmail.com>
2025-01-24 11:10:09 +0530

    Code commit

--- firebaseauth.js
+++ firebaseauth.js
@@ -4,8 +4,12 @@
  import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
  
  const firebaseConfig = {
- //YOUR COPIED FIREBASE PART SHOULD BE HERE
- //WATCH THIS VIDEO TO LEARN WHAT TO PUT HERE   https://youtu.be/_Xczf06n6x0
+    apiKey: "AIzaSyD4W29DBXR18opzTEZnEU2JNQTwt_32QnI",
+    authDomain: "mymakaut-c1246.firebaseapp.com",
+    projectId: "mymakaut-c1246",
+    storageBucket: "mymakaut-c1246.firebasestorage.app",
+    messagingSenderId: "1037242765675",
+    appId: "1:1037242765675:web:90e55c518704d3b6177c2e"
  };
 
  // Initialize Firebase
